cf4136549702e1d8ceb283fa320951f7a833d10c
jor_rey13 <jr.reyes2014@gmail.com>
2023-10-13 15:56:02 -0600

    examennivel4

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $usuarios = Usuario::all();
+        return response()->json($usuarios);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $usuario = new Usuario([
+            'nombre' => $data['nombre'],
+            'apellido' => $data['apellido'],
+            'correo' => $data['correo'],
+        ]);
+
+        $usuario->save();
+
+        return response()->json(['message' => 'Usuario creado exitosamente'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $usuario = Usuario::find($id);
+        return response()->json($usuario);
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $usuario = Usuario::find($id);
+
+        if (!$usuario) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        $usuario->nombre = $data['nombre'];
+        $usuario->apellido = $data['apellido'];
+        $usuario->correo = $data['correo'];
+
+        $usuario->save();
+
+        return response()->json(['message' => 'Usuario actualizado exitosamente']);
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $usuario = Usuario::find($id);
+        if (!$usuario) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        $usuario->delete();
+        return response()->json(['message' => 'Usuario eliminado'], 200);
+    }
+}
